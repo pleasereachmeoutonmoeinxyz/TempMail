d28c0aff33c34ed6e54ac397c2febdbb83ee65ce
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2014-04-07 23:04:38 +0430

    Add template

---
+++ web/template/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Temp Mail</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/jquery.poptrox.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+                <script src="js/get.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+
+				<!-- Logo -->
+					<h1 id="logo"><a href="#">Temp Mail</a></h1>
+				
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="#get">Get Temp Mail Address</a></li>
+							<li><a href="#whatisthis">What Is Temp Mail?</a></li>
+							<li><a href="#contact">Contact</a></li>
+						</ul>
+					</nav>
+
+			</header>
+			
+		<!-- Intro -->
+			<section id="get" class="main style1 dark fullscreen">
+				<div class="content container small">
+					<header>
+						<h2>Forward All To ...</h2>
+					</header>
+                                        <form id="mailform">
+                                                <div class="row half">
+                                                    <div class="12u"><input type="text" name="email" placeholder="Your current mail " /></div>
+                                                    <div class="12u" id="error"></div>
+                                                </div>
+                                                <div class="row">
+                                                        <div class="12u">
+                                                                <ul class="actions">
+                                                                    <li><input type="button" class="button" value="Get Temp Address For Next 120 Mins" /></li>
+                                                                </ul>
+                                                        </div>
+                                                </div>
+                                        </form>
+					<footer>
+						<a href="#whatisthis" class="button style2 down">More</a>
+					</footer>
+				</div>
+			</section>
+		
+		<!-- One -->
+			<section id="whatisthis" class="main style2 right dark fullscreen primary">
+				<div class="content box style2">
+					<header>
+						<h2>What Is Temp Mail?</h2>
+					</header>
+					<p>Temp Mail is temporary mail provider to protect you against spam, phishing and other online abuses.It forwards mail to your email address only for 2 hours after getting new temp mail. </p>
+				</div>
+				<a href="#contact" class="button style2 down anchored">Next</a>
+			</section>
+                        
+                        <section id="work" class="">
+			</section>
+		<!-- Contact -->
+			<section id="contact" class="main style3 secondary">
+				<div class="content container">
+					<header>
+                                                <h2>Any question?</h2>
+                                                <p>Feel free to contact me about <b>Temp Mail</b> on</p>
+                                                <br>
+                                                <h2>moein7tl [@atsign] gmail [.dot] com</h2>
+					</header>
+				</div>
+			</section>
+			
+		<!-- Footer -->
+			<footer id="footer">
+			
+
+				<!-- Menu -->
+					<ul class="menu">
+                                            <li>Developer : <a href="http://moeinhm.ir">Moein Hosseini</a></li>
+						<li>Theme : <a href="http://html5up.net/">HTML5 UP</a></li>
+					</ul>
+			
+			</footer>
+
+	</body>
+</html>
